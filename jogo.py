010e1e2bd3151bc348c5661339280c46882f8d08
Marcos Manfre <marcosmanfre@hotmail.com>
2021-12-23 09:00:59 -0300

    jogo1

--- jogo.py
+++ jogo.py
@@ -2,3 +2,15 @@
 print("********************************")
 print("Bem vendo ao jogo de Adivinhação!")
 print("********************************")
+
+
+numero_secreto = 42
+
+chute = input("Digite o seu número: ")
+
+print("Você digitou: ", chute)
+
+if numero_secreto == chute:
+    print("Você acertou")
+else:
+    print("Você errou")
